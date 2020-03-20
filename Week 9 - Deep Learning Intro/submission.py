f3bee8974fe920a4ddaa580e79c7c594be1a8c87
Landon <landonvolkmann@gmail.com>
2020-03-20 13:31:06 -0500

    Normalization attempt

--- Week 9 - Deep Learning Intro/submission.py
+++ Week 9 - Deep Learning Intro/submission.py
@@ -7,58 +7,58 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 
-
-# QUESTION 1
-# Adding Dense layers to the existing source code
-
-dataset = pd.read_csv("diabetes.csv", header=None).values
-
-X_train, X_test, Y_train, Y_test = train_test_split(dataset[:,0:8], dataset[:,8],
-                                                    test_size=0.25, random_state=87)
-np.random.seed(155)
-my_first_nn = Sequential() # create model
-my_first_nn.add(Dense(20, input_dim=8, activation='relu')) # hidden layer
-my_first_nn.add(Dense(1, activation='sigmoid')) # output layer
-my_first_nn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
-my_first_nn_fitted = my_first_nn.fit(X_train, Y_train, epochs=100,
-                                     initial_epoch=0)
-my_first_nn.summary()
-loss, accuracy = my_first_nn.evaluate(X_test, Y_test)
-
-print("LOSS: {}".format(loss))
-print("ACCURACY: {}".format(accuracy))
-
-# Now the same model again with more Dense layers
-
-np.random.seed(155)
-my_first_nn = Sequential() # create model
-my_first_nn.add(Dense(20, input_dim=8, activation='relu')) # hidden layer
-my_first_nn.add(Dense(20, input_dim=8, activation='relu')) # hidden layer
-my_first_nn.add(Dense(1, activation='sigmoid')) # output layer
-my_first_nn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
-my_first_nn_fitted = my_first_nn.fit(X_train, Y_train, epochs=100,
-                                     initial_epoch=0)
-my_first_nn.summary()
-loss2, accuracy2 = my_first_nn.evaluate(X_test, Y_test)
-
-print("NEW LOSS: {} CHANGE: {}".format(loss2, loss2 - loss))
-print("NEW ACCURACY: {} CHANGE: {}".format(accuracy2, accuracy2 - accuracy))
-
-# Now the same model again with more epochs
-
-np.random.seed(155)
-my_first_nn = Sequential() # create model
-my_first_nn.add(Dense(20, input_dim=8, activation='relu')) # hidden layer
-my_first_nn.add(Dense(20, input_dim=8, activation='relu')) # hidden layer
-my_first_nn.add(Dense(1, activation='sigmoid')) # output layer
-my_first_nn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
-my_first_nn_fitted = my_first_nn.fit(X_train, Y_train, epochs=150,
-                                     initial_epoch=0)
-my_first_nn.summary()
-loss3, accuracy3 = my_first_nn.evaluate(X_test, Y_test)
-
-print("NEW LOSS: {} CHANGE: {}".format(loss3, loss3 - loss2))
-print("NEW ACCURACY: {} CHANGE: {}".format(accuracy3, accuracy3 - accuracy2))
+#
+# # QUESTION 1
+# # Adding Dense layers to the existing source code
+#
+# dataset = pd.read_csv("diabetes.csv", header=None).values
+#
+# X_train, X_test, Y_train, Y_test = train_test_split(dataset[:,0:8], dataset[:,8],
+#                                                     test_size=0.25, random_state=87)
+# np.random.seed(155)
+# my_first_nn = Sequential() # create model
+# my_first_nn.add(Dense(20, input_dim=8, activation='relu')) # hidden layer
+# my_first_nn.add(Dense(1, activation='sigmoid')) # output layer
+# my_first_nn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+# my_first_nn_fitted = my_first_nn.fit(X_train, Y_train, epochs=100,
+#                                      initial_epoch=0)
+# my_first_nn.summary()
+# loss, accuracy = my_first_nn.evaluate(X_test, Y_test)
+#
+# print("LOSS: {}".format(loss))
+# print("ACCURACY: {}".format(accuracy))
+#
+# # Now the same model again with more Dense layers
+#
+# np.random.seed(155)
+# my_first_nn = Sequential() # create model
+# my_first_nn.add(Dense(20, input_dim=8, activation='relu')) # hidden layer
+# my_first_nn.add(Dense(20, input_dim=8, activation='relu')) # hidden layer
+# my_first_nn.add(Dense(1, activation='sigmoid')) # output layer
+# my_first_nn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+# my_first_nn_fitted = my_first_nn.fit(X_train, Y_train, epochs=100,
+#                                      initial_epoch=0)
+# my_first_nn.summary()
+# loss2, accuracy2 = my_first_nn.evaluate(X_test, Y_test)
+#
+# print("NEW LOSS: {} CHANGE: {}".format(loss2, loss2 - loss))
+# print("NEW ACCURACY: {} CHANGE: {}".format(accuracy2, accuracy2 - accuracy))
+#
+# # Now the same model again with more epochs
+#
+# np.random.seed(155)
+# my_first_nn = Sequential() # create model
+# my_first_nn.add(Dense(20, input_dim=8, activation='relu')) # hidden layer
+# my_first_nn.add(Dense(20, input_dim=8, activation='relu')) # hidden layer
+# my_first_nn.add(Dense(1, activation='sigmoid')) # output layer
+# my_first_nn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+# my_first_nn_fitted = my_first_nn.fit(X_train, Y_train, epochs=150,
+#                                      initial_epoch=0)
+# my_first_nn.summary()
+# loss3, accuracy3 = my_first_nn.evaluate(X_test, Y_test)
+#
+# print("NEW LOSS: {} CHANGE: {}".format(loss3, loss3 - loss2))
+# print("NEW ACCURACY: {} CHANGE: {}".format(accuracy3, accuracy3 - accuracy2))
 
 
 # QUESTION 2
@@ -96,7 +96,7 @@ print("ACCURACY: {}".format(accuracy))
 
 # Normalizing imports
 from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
+
 
 dataset_df = pd.read_csv("breastcancer.csv")
 
@@ -106,8 +106,16 @@ dataset_df["diagnosis"] = dataset_df["diagnosis"].replace('B', 0)
 dataset = dataset_df.values
 
 
-print(dataset[:,2:])
-print(dataset[:,1])
+# print(dataset[:,2:])
+# print(dataset[:,1])
+
+sc = StandardScaler().fit(dataset[:,2:])
+dataset[:,2:] = sc.transform(dataset[:,2:])
+
+# print(dataset[:,2:])
+# print(dataset[:,1])
+
+
 
 X_train, X_test, Y_train, Y_test = train_test_split(dataset[:,2:], dataset[:,1],
                                                     test_size=0.25, random_state=87)
@@ -119,7 +127,7 @@ my_first_nn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'
 my_first_nn_fitted = my_first_nn.fit(X_train, Y_train, epochs=100,
                                      initial_epoch=0)
 my_first_nn.summary()
-loss, accuracy = my_first_nn.evaluate(X_test, Y_test)
+loss2, accuracy2 = my_first_nn.evaluate(X_test, Y_test)
 
-print("LOSS: {}".format(loss))
-print("ACCURACY: {}".format(accuracy))
+print("NEW LOSS: {} CHANGE: {}".format(loss2, loss2 - loss))
+print("NEW ACCURACY: {} CHANGE: {}".format(accuracy2, accuracy2 - accuracy))
